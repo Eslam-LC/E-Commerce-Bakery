31d720b4647ec1356a0a7c8f2257bc86220aa7b9
Eslam Abd-Elsatar <eslamlegend5@gmail.com>
2026-03-14 22:08:18 +0200

    fix links

--- js/user.js
+++ js/user.js
@@ -86,6 +86,7 @@ function logout() {
         // updateAccountIcon()
     }
     updateAccountIcon()
+    window.location.href = '../index.html'
 }
 
 function updateAccountIcon() {
@@ -108,7 +109,7 @@ function updateAccountIcon() {
 function validateCredentials(email, password, onSuccess, onFail) {
     // not fully implemented yet
     var valid = false
-    fetchJSON('/api/users', (response) => {
+    fetchJSON('../api/users', (response) => {
         for (const u of response) {
             if (u.email == email && u.password == password) {
                 valid = true
