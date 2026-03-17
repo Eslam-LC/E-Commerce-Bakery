c184a061fd28e6e954e7cfc91c59013e8dc06279
Eslam Abd-Elsatar <eslamlegend5@gmail.com>
2026-03-17 15:26:35 +0200

    fix main page and products page header nav links

--- js/products-page.js
+++ js/products-page.js
@@ -2,9 +2,10 @@ updateCartBadge()
 updateAccountIcon()
 
 var allProducts = []
+var categoryParam = null
 
 var xhr = new XMLHttpRequest()
-xhr.open('GET', 'https://raw.githubusercontent.com/Eslam-LC/E-Commerce-Bakery/refs/heads/main/api/products')
+xhr.open('GET', './api/products')
 xhr.responseType = "json"
 xhr.send()
 
@@ -12,7 +13,15 @@ xhr.onload = function () {
 
     allProducts = xhr.response
 
-    renderGrid(allProducts)
+    const urlParams = new URLSearchParams(window.location.search)
+    categoryParam = urlParams.get('category')
+
+    let initialProducts = allProducts
+    if (categoryParam) {
+        initialProducts = allProducts.filter(p => p.category === categoryParam)
+    }
+
+    renderGrid(initialProducts)
     buildCategories(allProducts)
 }
 
@@ -86,6 +95,14 @@ function buildCategories(products) {
     }
 
     document.getElementById("search").addEventListener("input", applyFilters)
+
+    if (categoryParam) {
+        const checkbox = document.querySelector(`input.cat-filter[value="${categoryParam}"]`);
+        if (checkbox) {
+            checkbox.checked = true;
+        }
+        applyFilters();
+    }
 }
 
 function applyFilters() {
